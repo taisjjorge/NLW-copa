41efccedf04929fd245b3f25eb5c65ebb28b853b
tais.amanda <tais.amanda@g.globo>
2022-11-03 18:55:30 -0300

    feat: nlw-copa - configurações iniciais do projeto

---
+++ server/src/server.ts
@@ -0,0 +1,35 @@
+import Fastify from 'fastify';
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({
+    log: ['query'],
+})
+
+async function bootstrap() {
+    const fastify = Fastify({
+        logger: true,
+    })
+
+    await fastify.register(cors, {
+        origin: true,
+    })
+
+    fastify.get('/pools/count', async () => {
+        const count = await prisma.pool.count()
+        
+        // const pools = await prisma.pool.findMany({
+        //     where: {
+        //         code: {
+        //             startsWith: 'B'
+        //         }
+        //     }
+        // })
+
+        return { count }
+    })
+
+    await fastify.listen({ port: 3333, /*host: '0.0.0.0'*/ })
+}
+
+bootstrap()
